5ed2a1d4dccf0e0d03d5e2c56c33ad6ccdb08930
Seun-O <12373359+Seun-O@users.noreply.github.com>
2019-12-16 22:08:27 -0500

    Adding previous solutions

--- Short-Answer/Algorithms_Answers.md
+++ Short-Answer/Algorithms_Answers.md
@@ -2,14 +2,22 @@
 
 ## Exercise I
 
-a)
+a) runtime O(n) because the loops runtime scales linearly with the input N
 
 
-b)
+b) runtime O(n^2) There are two loops that will run in proportion to what N is
 
 
-c)
+c) runtime O(n) It is a recursive problem which would run N times
 
 ## Exercise II
+stories = number of floors
+f = max building before egg brakes
+
+for i in range(stories):
+     if i >= f:
+         print("egg broke")
+
+ Complexity = O(logn)
 
 
--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,8 +3,19 @@ Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
+
+
 def count_th(word):
-    
+
     # TBC
-    
-    pass
+    counter = 1
+    if len(word) >= 2:
+        if word[:2] == 'th':
+            counter += 1
+            return 1 + count_th(word[counter:])
+        else:
+            return count_th(word[counter:])
+    return 0
+
+
+print(count_th("theduckthe"))
