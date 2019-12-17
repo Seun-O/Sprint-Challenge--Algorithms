be7292317d8ca8e04352db243418519eced093a5
Seun-O <12373359+Seun-O@users.noreply.github.com>
2019-12-16 22:17:00 -0500

    Added robot sort

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -100,6 +100,26 @@ class SortingRobot:
         """
         # Fill this out
 
+        # Turn on the Light
+        self.set_light_on()
+        while self.light_is_on():
+            if not self.can_move_right():
+                if self.light_is_on():
+                    self.set_light_off()
+                    while self.can_move_left():
+                        self.move_left()
+                else:
+                    break
+
+            self.swap_item()
+            self.move_right()
+            if self.compare_item() == 1:
+                self.swap_item()
+                self.set_light_on()
+            self.move_left()
+            self.swap_item()
+            self.move_right()
+
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
